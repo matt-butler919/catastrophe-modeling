090562a53bd8205cdc41f9b2f39e1206f13fbf02
matt-butler919 <mattbutler0003@gmail.com>
2026-05-26 00:27:12 -0400

    Delete Annual Disaster Summary.sql

--- Annual Disaster Summary.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    SUBSTR(declarationDate, 1, 4) AS year,
-    incidentType,
-    COUNT(DISTINCT disasterNumber) AS disaster_count
-FROM disaster_declarations
-WHERE SUBSTR(declarationDate, 1, 4) >= '2000'
-GROUP BY year, incidentType
-ORDER BY year, disaster_count DESC;
